918c3dece870f035535d663127e793a0ae199f6e
陈品艺 <baiyikong@foxmail.com>
2023-07-24 10:07:24 +0800

    1.两数之和

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,23 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println(200);
+     int temp1 = (int)Math.pow(2,31);
+     int temp2 = (int)Math.pow(10,9);
+
+        System.out.println(temp1);
+        System.out.println(temp2);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        //return 代码优化
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if((nums[i]+nums[j])==target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[0];
+
+        //TODO 用hashmap来优化该题
     }
 }
