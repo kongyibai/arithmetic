c82320d7e0d9829aeefe72ade424049e6886720d
陈品艺 <baiyikong@foxmail.com>
2023-09-08 17:00:49 +0800

    算法项目重新初始化

--- src/main/java/com/baiyikong/sort/SortDemo1.java
+++ src/main/java/com/baiyikong/sort/SortDemo1.java
@@ -1,7 +1,10 @@
-package arithmetic.src.main.java.com.baiyikong.sort;
+package com.baiyikong.sort;
 
 public class SortDemo1 {
 
-
+    public static void main(String[] args) {
+        //用java来实现一下快速排序
+        System.out.println("hello world");
+    }
 
 }
